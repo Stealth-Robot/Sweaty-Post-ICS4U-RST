f2551e077345d76534d4dbd0e9c0edf7f5961105
Connor Adams <cnnr252@gmail.com>
2018-05-23 14:27:15 -0400

    rectangle hit box

---
+++ src/gui/objects/Rectangle.java
@@ -0,0 +1,36 @@
+package gui.objects;
+
+import gui.Vector2;
+
+public class Rectangle {
+
+	private int length;
+	private int width;
+	private Vector2 position;
+	public Rectangle(int length, int width, Vector2 position) {
+		this.length = length;
+		this.width = width;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public Vector2 getPosition() {
+		return position;
+	}
+	public void setPosition(Vector2 position) {
+		this.position = position;
+	}
+	public boolean getInside(Vector2 insideCheck) {
+		return insideCheck.x > position.x && insideCheck.x < position.x + length 
+				&& insideCheck.y > position.y && insideCheck.y < position.y + width;
+	}
+}
