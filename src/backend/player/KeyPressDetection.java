8740b45f887d40d4bfcf0a8437cf979c454463b7
Grayden Hibbert <graydenhibbert@hotmail.com>
2018-05-28 17:56:40 -0400

    Completly rewrote the movement code
    Rewrote the movement code so that it can output either the x or the y variable. Need to find out where i import the movement code and where I set the movement to the player

--- src/backend/player/KeyPressDetection.java
+++ src/backend/player/KeyPressDetection.java
@@ -1,27 +1,92 @@
 package backend.player;
-
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class KeyPressDetection extends KeyAdapter{
+public class KeyPressDetection extends KeyAdapter {
 
-	public int KeyPressHor(KeyEvent event) {
-		int x = 0;
-		char ch = event.getKeyChar();
-		if (ch == 'a' ||ch == 'd') {
-			if (ch == 'a') x++;
-			else x--;
+	// checks if WASD key is pressed down
+	public boolean LEFT_KEY_DOWN = false;
+	public boolean RIGHT_KEY_DOWN = false;
+	public boolean UP_KEY_DOWN = false;
+	public boolean DOWN_KEY_DOWN = false;
+
+	/**
+	 * Sets the WASD boolean value to true if the key is pressed down
+	 * @param e key event.
+	 */
+	public void keyPressed(KeyEvent e) {
+		int keys = e.getKeyCode();
+
+		if (keys == KeyEvent.VK_D) {
+			RIGHT_KEY_DOWN = true;
+		} if (keys == KeyEvent.VK_A) {
+			LEFT_KEY_DOWN = true;
+		} if (keys == KeyEvent.VK_W) {
+			UP_KEY_DOWN = true;
+		} if (keys == KeyEvent.VK_S) {
+			DOWN_KEY_DOWN = true;
 		}
-		return x;
 	}
 
-	public int KeyPressVer(KeyEvent event)  {
-		int y = 0;
-		char ch = event.getKeyChar();
-		if (ch == 's'||ch == 'w') {
-			if (ch == 's') y = -1;
-			else y--;
-		} else y = 0;
-		return y;
+	/**
+	 * Sets the WASD boolean value to false if the key is released
+	 * @param e key event.
+	 */
+	public void keyReleased(KeyEvent e) {
+		int keys = e.getKeyCode();
+
+		if (keys == KeyEvent.VK_D) {
+			RIGHT_KEY_DOWN = false;
+		} if (keys == KeyEvent.VK_A) {
+			LEFT_KEY_DOWN = false;
+		} if (keys == KeyEvent.VK_W) {
+			UP_KEY_DOWN = false;
+		} if (keys == KeyEvent.VK_S) {
+			DOWN_KEY_DOWN = false;
+		}
+	}
+
+	/**
+	 * Creates a KeyEvent to be used every tick
+	 * @param e key event.
+	 */
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	/**
+	 * Calculates the x & y value in terms of movement direction
+	 * 
+	 * @param isX true if x value is wanted. false if y value is wanted.
+	 * @return
+	 */
+	public int processKeyEvent(boolean isX) {
+		int x = 0, y = 0;
+		if (UP_KEY_DOWN) y++;
+		if (DOWN_KEY_DOWN) y--;
+		if (LEFT_KEY_DOWN) x--;
+		if (RIGHT_KEY_DOWN) x++;
+
+
+//		System.out.println(x + " | " + y);
+
+		if (isX) {
+			return x;
+		}
+		else return y;
+		//Call Movement stuffs
+//		if (y == 1) {
+//			if (x == 1) return NORTH_EAST;
+//			else if (x == -1) return NORTH_WEST;
+//			else return NORTH;
+//		} else if (y == -1) {
+//			if (x == 1) return SOUTH_EAST;
+//			else if (x == -1) return SOUTH_WEST;
+//			else return SOUTH;
+//		} else {
+//			if (x == 1) return EAST;
+//			else if (x == -1) return WEST;
+//			else return NONE;
+//		} 
 	}
 }
