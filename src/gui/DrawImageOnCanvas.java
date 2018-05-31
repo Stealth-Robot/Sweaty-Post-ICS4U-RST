e1c0d5d7cf8c3418bcf249d72e7852afe14c08e1
Stealth-Robot <kubiliusnick8@gmail.com>
2018-05-31 13:47:22 -0400

    Added Images and editing randomization of images

--- src/gui/DrawImageOnCanvas.java
+++ src/gui/DrawImageOnCanvas.java
@@ -80,32 +80,51 @@ public class DrawImageOnCanvas {
         testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
         for(int x = 0; x < 9; x++) {
         	for(int y = 0; y < 6; y++) {
-        		int worldGen = (int) (12 * Math.random() + 1);
-		        switch (worldGen)  //switches are used to set the tile based on a random number (1-6)
-		        {
-		            case 1:  
-		                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
-	        		break;
-		            case 2:  
-		                testImage = ImageLoader.loadImage("src/images/sprites/environments/DesertRock.png");
-	        		break;
-		            case 3:  
-		                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
-	        		break;
-		            case 4:  
-		                testImage = ImageLoader.loadImage("src/images/sprites/environments/DesertFlower.png");
-	        		break;
-		            case 5:  
-		                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
-	        		break;
-		            default:  
-		                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
-	        		break;
-		        }	        		  
-                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
-        		
+        		if(y>0) {
+	        		int worldGen = (int) (12 * Math.random() + 1);
+			        switch (worldGen)  //switches are used to set the tile based on a random number (1-6)
+			        {
+			            case 1:  
+			                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert3.png");
+		        		break;
+			            case 2:  
+			                testImage = ImageLoader.loadImage("src/images/sprites/environments/DesertRock.png");
+		        		break;
+			            case 3:  
+			                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert2.png");
+		        		break;
+			            case 4:  
+			                testImage = ImageLoader.loadImage("src/images/sprites/environments/DesertFlower.png");
+		        		break;
+			            case 5:  
+			                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert2.png");
+		        		break;
+			            default:  
+			                testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
+		        		break;
+			        }
+        		}
+        		else
+        		{
+        			int railGen = (int) (6 * Math.random() + 1);
+    		        switch (railGen)  //switches are used to set the tile based on a random number (1-6)
+    		        {
+    		            case 1:  
+    		                testImage = ImageLoader.loadImage("src/images/sprites/environments/RailBroken1.png");
+    	        		break;
+    		            case 2:  
+    		                testImage = ImageLoader.loadImage("src/images/sprites/environments/RailBroken2.png");
+    	        		break;
+    		            default:  
+    		                testImage = ImageLoader.loadImage("src/images/sprites/environments/Rail.png");
+    	        		break;
+    		        }
+    		        if(x==8)
+    		        	testImage = ImageLoader.loadImage("src/images/sprites/environments/Desert.png");
+    		        if(x==7)
+    		        	testImage = ImageLoader.loadImage("src/images/sprites/environments/RailEnd.png");
+        		}
 		        environment.add(new ImageData(testImage,x,y));
-	            	
         	}
         }
     }
