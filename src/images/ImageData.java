e7e5f630588da834e2b20aaf7e1df909e10af822
Connor Adams <cnnr252@gmail.com>
2018-05-31 14:10:47 -0400

    Flipping character image possible

--- src/images/ImageData.java
+++ src/images/ImageData.java
@@ -1,6 +1,8 @@
 package images;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 import gui.Display;
 
@@ -10,13 +12,48 @@ public class ImageData {
 	private int xPos;
 	private int yPos;
 	
+	private Image rightImage;
+	private Image downImage;
+	private Image leftImage;
 	public ImageData(Image image, int xPos, int yPos) {
 		this.image = image;
 		this.xPos = xPos * Display.PIXEL_IMAGE_SIZE;
 		this.yPos = (Display.SCREEN_HEIGHT - Display.PIXEL_IMAGE_SIZE) - (yPos * Display.PIXEL_IMAGE_SIZE);
 		this.image = image.getScaledInstance(Display.PIXEL_IMAGE_SIZE, Display.PIXEL_IMAGE_SIZE, Image.SCALE_DEFAULT);
+		rightImage = rotateCw(toBufferedImage(this.image));
+		downImage = rotateCw(toBufferedImage(rightImage));
+		leftImage = rotateCw(toBufferedImage(downImage));
 	}
+	public static BufferedImage rotateCw( BufferedImage img )
+	{
+	    int         width  = img.getWidth();
+	    int         height = img.getHeight();
+	    BufferedImage   newImage = new BufferedImage( height, width, img.getType() );
 
+	    for( int i=0 ; i < width ; i++ )
+	        for( int j=0 ; j < height ; j++ )
+	            newImage.setRGB( height-1-j, i, img.getRGB(i,j) );
+
+	    return newImage;
+	}
+	public static BufferedImage toBufferedImage(Image img)
+	{
+	    if (img instanceof BufferedImage)
+	    {
+	        return (BufferedImage) img;
+	    }
+
+	    // Create a buffered image with transparency
+	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+	    // Draw the image on to the buffered image
+	    Graphics2D bGr = bimage.createGraphics();
+	    bGr.drawImage(img, 0, 0, null);
+	    bGr.dispose();
+
+	    // Return the buffered image
+	    return bimage;
+	}
 	public Image getImage() {
 		return image;
 	}
@@ -39,6 +76,15 @@ public class ImageData {
 
 	public void setyPos(int yPos) {
 		this.yPos = yPos;
+	}
+	public Image getRightImage() {
+		return rightImage;
+	}
+	public Image getDownImage() {
+		return downImage;
+	}
+	public Image getLeftImage() {
+		return leftImage;
 	}
 	
 }
