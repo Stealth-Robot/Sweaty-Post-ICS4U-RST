00746395417302d1d01459d59767f73869b81c9c
Stealth-Robot <kubiliusnick8@gmail.com>
2018-06-05 14:18:37 -0400

    fixed merge conflict

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,23 +7,16 @@ import gui.Vector2;
 import gui.objects.Rectangle;
 import gui.objects.RectangleCreator;
 import gui.objects.colliders.Collider;
-<<<<<<< HEAD
 import wildWest.WildWest;
 
 import java.lang.Math;
-=======
->>>>>>> 6058bc87bcd642ce36fe0313a54093a419dc4893
 //update
 public class Main {
 	public static ArrayList<Rectangle> sceneColliders = new ArrayList<Rectangle>();
 	public static DrawImageOnCanvas game;
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		game = new DrawImageOnCanvas();
+		sceneColliders.add(RectangleCreator.createDimensionalRectangle(1,1,2,2));
 		new WildWest();
-=======
-		   game = new DrawImageOnCanvas();
-		   sceneColliders.add(RectangleCreator.createDimensionalRectangle(1,1,2,2));
->>>>>>> 6058bc87bcd642ce36fe0313a54093a419dc4893
 	}
 }
