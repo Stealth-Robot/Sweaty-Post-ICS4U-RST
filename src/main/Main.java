a666040124e87945cf37d4caa06b9cf49bce4db2
Stealth-Robot <kubiliusnick8@gmail.com>
2018-06-05 14:55:53 -0400

    Fixed connors MERGE errors that Connor was too fucking lazy to fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,25 +7,18 @@ import gui.Vector2;
 import gui.objects.Rectangle;
 import gui.objects.RectangleCreator;
 import gui.objects.colliders.Collider;
-<<<<<<< HEAD
 import gui.objects.colliders.SceneChangeCollider;
 import gui.scenes.locations.SceneMaster;
-=======
 import wildWest.WildWest;
 
 import java.lang.Math;
->>>>>>> 354e1a5a94f3f3100093d91a00e1b7a6370ad1f9
 //update
 public class Main {
 	
 	public static DrawImageOnCanvas game;
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		   game = new DrawImageOnCanvas();
-=======
 		game = new DrawImageOnCanvas();
-		sceneColliders.add(RectangleCreator.createDimensionalRectangle(1,1,2,2));
+		//sceneColliders.add(RectangleCreator.createDimensionalRectangle(1,1,2,2));
 		new WildWest();
->>>>>>> 354e1a5a94f3f3100093d91a00e1b7a6370ad1f9
 	}
 }
