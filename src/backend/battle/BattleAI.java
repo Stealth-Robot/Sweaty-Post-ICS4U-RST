354e1a5a94f3f3100093d91a00e1b7a6370ad1f9
Grayden Hibbert <graydenhibbert@hotmail.com>
2018-06-05 14:20:45 -0400

    Started adding backend support to WWW
    we should probably shot swearing sdo FUCKING MUCH in our code otherwise we might get in trougble. ALSO FUCK MARCUS

--- src/backend/battle/BattleAI.java
+++ src/backend/battle/BattleAI.java
@@ -1,4 +1,11 @@
 package backend.battle;
 
 public class BattleAI {
+	public int choice() {
+	int temp;
+		do 
+			temp = ((int)(Math.random() * 3) + 1);
+		while (temp != 4);
+		return temp;
+	}
 }
--- src/backend/battle/BattleCharacter.java
+++ src/backend/battle/BattleCharacter.java
@@ -1,34 +1,81 @@
 package backend.battle;
 
 public class BattleCharacter {
-	private int bullets;
+	int bullets;
 	private int maxBullets;
 	private boolean armored;
 	private boolean shot;
 	int turn;
+
+	/**
+	 * Called when the battle first starts, a new battle character is created
+	 * and that battle character's maximum ammo is chosen
+	 * @param maxBullets
+	 */
 	public BattleCharacter(int maxBullets) {
 		this.maxBullets = maxBullets;
 		armored = false;
 		shot = false;
 	}
-	
-	public void tick() {
-		armored = false;
+
+	/**
+	 * Called after the chracter makes their choice
+	 * @param enemy
+	 */
+	public void tick(BattleCharacter enemy) {
 		if (bullets > maxBullets) {
 			bullets = maxBullets;
-		}
+		} 
+		shooting(enemy);
 	}
-	
-	public void reload() {
+
+	/**
+	 * Called at the end of each character's turn
+	 */
+	public void resetTurn() {
+		armored = false;
+		shot = false;
+	}
+
+	/**
+	 * Calls the action chosen to perform
+	 * @param choice (1 = reload, 2 = shoot, 3 = defend)
+	 */
+	public void actions(int choice) {
+		if (choice == 1) reload();
+		if (choice == 2) shoot();
+		if (choice == 3) defend();
+	}
+
+	private void reload() {
 		bullets++;
 	}
-	
-	public void defend() {
+	private void defend() {
 		armored = true;
 	}
-	
-	public void shoot() {
-		bullets--;
-		shot = true;
+	private void shoot() {
+		if (bullets > 0) {
+			bullets--;
+			shot = true;
+		}
+	}
+
+	/**
+	 * Calls the getShot function from the enemy battleCharacter
+	 * @param enemy
+	 * @return
+	 */
+	public boolean shooting(BattleCharacter enemy) {
+		return (enemy.getShot());
+	}
+
+	/**
+	 * Called from another battleCharacter
+	 * @return
+	 */
+	public boolean getShot() {
+		if (shot) return false;
+		else if (armored) return false;
+		else return true;
 	}
 }
