42fecee8365ad82779e08f39d134d1467d0181b1
Stealth-Robot <kubiliusnick8@gmail.com>
2018-06-12 11:55:32 -0400

    commented out Grayden's useless stuff

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,6 +20,6 @@ public class Main {
 	public static void intro () {
 		intro = "Welcome to Sweaty Post! you have accadently released a bunch of bad bois and you have been tasked with recatching them."
 				+ "\nAlso try not to die while doing so because that means you will lose";
-		DialogCreator.createDialog("The Welcome Wagon of Sweaty Post", intro);
+		//DialogCreator.createDialog("The Welcome Wagon of Sweaty Post", intro);
 	}
 }
