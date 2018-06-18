6d9c609ceee55529e3a554bb1b4bbe1895ac5097
Grayden Hibbert <graydenhibbert@hotmail.com>
2018-06-18 09:23:30 -0400

    Commented the Battle AI

--- src/backend/battle/BattleAI.java
+++ src/backend/battle/BattleAI.java
@@ -12,27 +12,36 @@ public class BattleAI {
 	 * post: ai chooses action
 	 */
 	public static int choice(BattleCharacter player, BattleCharacter enemy) {
+		//Adds weight to the shooting action
 		int shoot = 0;
+		//Weights the shooting option more if there are more bullets
 		for(int i = 0; i < enemy.bullets; i++) {
 			if(Math.random() < 0.5) {
 				shoot++;
 			}
 		}
+		//Adds weight to the blocking option
 		int block = 0;
+		//Weights the blocking option more if there are more player bullets
 		for(int i = 0; i < player.bullets; i++) {
 			if(Math.random() < 0.5) {
 				block++;
 			}
 		}
+		//if there is a higher shooting chance than zero and there is more shooting than block
 		if(shoot > 0 && shoot > block) {
 			return 1;
 		}
+		//If block has a higher outcome
 		else if(block > 0 && block > shoot) {
 			return 3;
 		}
+		//If they are equal and above then choose randomly
 		else if(shoot > 0 && block > 0) {
 			return Math.random() < 0.5 ? 1 : 3;
 		}
+		
+		//Otherwise reload
 		return 2;
 	}
 }
